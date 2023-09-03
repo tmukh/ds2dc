bfe773a9148533388e6a1b42d89d2f5740251a13
tmukh <derpender123@gmail.com>
2023-09-03 10:02:10 +0200

    Added name error handling

--- smartConvDS/conversion_scripts/convert.py
+++ smartConvDS/conversion_scripts/convert.py
@@ -1,8 +1,12 @@
-import os, sys
+import os
+import sys
 import time
+import re  # Import the regular expression module for sanitization
 from conversion_scripts import TabularConvert, graphConvert, convertDocKV, documentConvert
 from Testing import convert_to_redis
+
 root_folder = sys.argv[-1]
+
 # Dictionary to map extension types to their corresponding conversion functions
 conversion_info = {
     'tabular': (TabularConvert.convert_to_csv_parallel, None),
@@ -10,6 +14,12 @@ conversion_info = {
     'keyvalue': (convertDocKV.convert_to_json_and_txt, 'kv_files'),
     'document': (convertDocKV.convert_to_json_and_txt, 'doc')
 }
+
+# Define a function to sanitize file names
+def sanitize_file_name(file_name):
+    # Replace invalid characters with underscores (you can customize this as needed)
+    sanitized_name = re.sub(r'[\\/:*?"<>|]', '_', file_name)
+    return sanitized_name
 
 def convert_files(paths, extension_type, extension_list):
     if extension_type not in ['tabular', 'graph', 'keyvalue', 'document']:
@@ -24,12 +34,18 @@ def convert_files(paths, extension_type, extension_list):
                     file_extension = os.path.splitext(file)[-1]
                     if file_extension in target_extensions:
                         input_file = os.path.join(root, file)
-                        convert_file(input_file, extension_type)
+                        sanitized_file_name = sanitize_file_name(file)  # Sanitize the file name
+                        sanitized_input_file = os.path.join(root, sanitized_file_name)
+                        os.rename(input_file, sanitized_input_file)  # Rename the file
+                        convert_file(sanitized_input_file, extension_type)
         elif os.path.isfile(path):
             file_extension = os.path.splitext(path)[-1]
             if file_extension in target_extensions:
                 input_file = path
-                convert_file(input_file, extension_type)
+                sanitized_file_name = sanitize_file_name(os.path.basename(input_file))  # Sanitize the file name
+                sanitized_input_file = os.path.join(os.path.dirname(input_file), sanitized_file_name)
+                os.rename(input_file, sanitized_input_file)  # Rename the file
+                convert_file(sanitized_input_file, extension_type)
         else:
             print(f"Invalid path: {path}")
 
@@ -48,3 +64,4 @@ def convert_file(input_file, extension_type):
     else:
         print(f"Invalid extension type: {extension_type}")
 
+        
